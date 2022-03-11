103877ae070b67de3e837428177afa1fadaa2adf
Vitor Hugo <vitorhprsilva@gmail.com>
2022-03-11 12:45:09 -0300

    feat: include input in checkout page

--- src/views/Checkout/index.js
+++ src/views/Checkout/index.js
@@ -1,7 +1,24 @@
+import {InputText} from '../../components'
+import { validatesMask, cardNumberMask, removeNumbers, cvvMask } from '../../utils'
+import { Link } from 'react-router-dom'
 
 export const Checkout = () => {
   return (
-      <div style={{backgroundColor: 'blue', flex: 1}} > teste </div>
+      <div style={{flex: 1}}> 
+        <div className='div-card'>
+          {/* <Link to='/teste' >Alterar forma de pagamento</Link> */}
+          Alterar forma de pagamento
+          Adicione um novo cartão de crédito
+        </div>
+        <div>
+          <InputText placeholder='Número do cartão' maskFunction={cardNumberMask} onChangeText={()=>{}} /> 
+          <InputText placeholder='Nome (igual ao cartão)' maskFunction={removeNumbers} onChangeText={()=>{}} />
+          <div>
+            <InputText placeholder='Validade' maskFunction={validatesMask} onChangeText={()=>{}} /> 
+            <InputText placeholder='CVV' maskFunction={cvvMask} onChangeText={()=>{}} /> 
+          </div> 
+        </div>
+      </div>
   );
 }
 
