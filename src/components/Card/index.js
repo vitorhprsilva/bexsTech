b9d79b9c4a70562296ee46f69b93f18b6be0a913
Vitor Hugo <vitorhprsilva@gmail.com>
2022-03-11 14:18:24 -0300

    fix: problem tests by text resolved

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -23,10 +23,10 @@ export const Card = ({numberCard, name, validate}) => {
   return(
     <div className={number===null ? 'card-off' : 'card-on'}>
       <div className="datas">
-        <text className="text-card">{number===null ? '**** **** **** ****' : number }</text>
+        <p className="text-card">{number===null ? '**** **** **** ****' : number }</p>
         <div className="datas-name-validate">
-          <text className="text-card">{nameTitular===null ? 'Nome do Titular' : nameTitular }</text>
-          <text className="text-card">{validateCard===null ? '00/00' : validateCard }</text>
+          <p className="text-card">{nameTitular===null ? 'Nome do Titular' : nameTitular }</p>
+          <p className="text-card">{validateCard===null ? '00/00' : validateCard }</p>
         </div>
       </div>
     </div>
