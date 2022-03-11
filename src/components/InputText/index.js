7ebf52437c28c0d0d4d3fa5eed3f35695bbfc768
Vitor Hugo <vitorhprsilva@gmail.com>
2022-03-11 09:58:44 -0300

    feat: component input text resolve test

---
+++ src/components/InputText/index.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+import './styles.css'
+
+export const InputText = ({placeholder, onChangeText, maskFunction}) => {
+
+  const [text, setText] = useState('')
+
+  const handleChange = (text) => {
+    const parseValue = (value) => {
+      if (maskFunction) {
+        return maskFunction(value, setText)
+      }
+      return text
+    }
+
+    const parsedText = parseValue(text)
+
+    if (onChangeText) {
+      onChangeText(parsedText)
+    }
+
+    return parsedText
+  }
+
+  return (
+      <input 
+        placeholder={placeholder} 
+        name={placeholder} 
+        onChange={(text) => {
+          const newText = handleChange(text.target.value)
+          onChangeText(newText)
+          setText(newText)
+        }} 
+        value={text}
+        className='input'
+      />
+  );
+}
